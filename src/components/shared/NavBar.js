570f65311145a8aa27e0e63c1f380e37654bcc42
RJ <jensenrs@gmail.com>
2018-02-15 10:21:32 -0800

    dropdown menu toggles show/hide

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
 const NavBar = () => {
+
+  function toggleHide(e){
+    const projectDropDown = document.querySelector('.project-menu')
+
+    projectDropDown.classList.contains('hide') ? projectDropDown.classList.remove('hide') : projectDropDown.classList.add('hide')
+  }
+
   return (
     <div>
       <nav>
         <p className="brand">ProtoCard</p>
-        <div className="project-name-wrapper">
+        <div className="project-name-wrapper" onClick={toggleHide}>
           <h2 className="project-name">PROJECT NAME</h2>
           <i className="project-drop-down material-icons">arrow_drop_down</i>
         </div>
         <i className="account material-icons">account_box</i>
       </nav>
-      <div className="project-menu">
+
+      <div className="project-menu hide">
         <div className="project-menu-item">Current CSV file: csfFileName.csv</div>
         <div className="divider"></div>
         <div className="local-action project-menu-item" id="uploadCSV">
