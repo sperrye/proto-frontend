7f10b04c11a1e05dec4abaa7d882fd1848dacd27
RJ <jensenrs@gmail.com>
2018-02-17 21:38:24 -0800

    minor style changes before branch

--- src/components/sidebars/CardActions.js
+++ src/components/sidebars/CardActions.js
@@ -1,28 +1,31 @@
 import React from 'react'
 
-const CardActions = () => (
-  <div className="m-1 card-actions">
-    <div className="mtb-05 local-action">
-      <i className="material-icons">check_box</i>
-      <span>Select All</span>
+const CardActions = () => {
+
+  return (
+    <div className="m-1 card-actions">
+      <div className="mtb-05 local-action">
+        <i className="material-icons">check_box</i>
+        <span>Select All</span>
+      </div>
+      <div className="mtb-05 local-action">
+        <i className="material-icons">check_box_outline_blank</i>
+        <span>Unselect All</span>
+      </div>
+      <div className="mtb-05 local-action" id="add-card">
+        <i className="material-icons">add</i>
+        <span>Add Card</span>
+      </div>
+      <div className="mtb-05 local-action">
+        <i className="material-icons">print</i>
+        <span>Print Selected</span>
+      </div>
+      <div className="mtb-05 local-action">
+        <i className="material-icons">delete</i>
+        <span>Delete Selected</span>
+      </div>
     </div>
-    <div className="mtb-05 local-action">
-      <i className="material-icons">check_box_outline_blank</i>
-      <span>Unselect All</span>
-    </div>
-    <div className="mtb-05 local-action">
-      <i className="material-icons">add</i>
-      <span>Add Card</span>
-    </div>
-    <div className="mtb-05 local-action">
-      <i className="material-icons">print</i>
-      <span>Print Selected</span>
-    </div>
-    <div className="mtb-05 local-action">
-      <i className="material-icons">delete</i>
-      <span>Delete Selected</span>
-    </div>
-  </div>
-)
+  )
+}
 
 export default CardActions
